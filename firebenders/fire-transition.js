6e43a97d1c98590ed6304a8fa334c2ccb266a5de
Alourne <120846738+Alourne@users.noreply.github.com>
2023-06-02 05:35:45 -0400

    added new images and finished card flips

--- firebenders/fire-transition.js
+++ firebenders/fire-transition.js
@@ -20,29 +20,52 @@
 //     }
 // }
 
-function changeImage() {
+function changeImageZuko() {
     let displayImage = document.getElementById("image1");
-    if (displayImage.src.match("images/Aang_air.jpg")) {
-        displayImage.src = "images/monkgyatso.jpg"
+    if (displayImage.src.match("/images/Zuko.jpg")) {
+        displayImage.src = "/images/ZukoSolo.jpg"
+    } else if(displayImage.src.match("/images/ZukoSolo.jpg")) {
+        displayImage.src = "/images/Zuko&Mai.jpg"
+    } else if(displayImage.src.match("/images/Zuko&Mai.jpg")) {
+        displayImage.src = "/images/Zuko2.jpg"
+    } else if(displayImage.src.match("/images/Zuko2.jpg")) {
+        displayImage.src = "/images/Zuko3.jpg"
     } else {
-        displayImage.src = "images/Aang_air.jpg"
+        displayImage.src = "/images/Zuko.jpg"
     }
 }
 
-function changeImageYang() {
+function changeImageIroh() {
     let displayImage = document.getElementById("image2");
-    if (displayImage.src.match("images/Aang_air.jpg")) {
-        displayImage.src = "images/yangchen.jpg" 
+    if (displayImage.src.match("/images/Iroh.jpg")) {
+        displayImage.src = "/images/iroh2.jpg"
+    } else if (displayImage.src.match("/images/iroh2.jpg")) {
+        displayImage.src = "/images/zuko&iroh.jpg"
     } else {
-        displayImage.src = "images/Aang_air.jpg"
+        displayImage.src = "/images/Iroh.jpg"
     }
 }
 
-function changeImageAang() {
+function changeImageAzula() {
     let displayImage = document.getElementById("image3");
-    if (displayImage.src.match("images/Aang_air.jpg")) {
-        displayImage.src = "images/AangGif.gif" 
+    if (displayImage.src.match("/images/azulafix.png")) {
+        displayImage.src = "/images/azula3.png" 
+    } else if (displayImage.src.match("/images/azula3.png")) {
+        displayImage.src = "/images/Azula5.jpg"
+    } else if (displayImage.src.match("/images/Azula5.jpg")) {
+        displayImage.src = "/images/azula4.jpg"
     } else {
-        displayImage.src = "images/Aang_air.jpg"
+        displayImage.src = "/images/azulafix.png"
+    }
+}
+
+function changeImageOzai() {
+    let displayImage = document.getElementById("image4");
+    if (displayImage.src.match("/images/Ozai.jpg")) {
+        displayImage.src = "/images/ozai2.jpg"
+    } else if(displayImage.src.match("/images/ozai2.jpg")) {
+        displayImage.src = "/images/firelord.gif"  
+    } else {
+        displayImage.src = "/images/Ozai.jpg"
     }
 }
